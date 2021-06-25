901a92c9d1a98b640e1e8ca4db206010461ad628
Kristýna Marvalová <kristyna.sulcova@cenia.cz>
2021-06-25 19:50:23 +0200

    headers first attempt

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  console.log(req.ip);
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+});
+
 
 
 // listen for requests :)
